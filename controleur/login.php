bb586b39af47a5b00c853d5c4fd4405b1c05674c
DclFabienLycan <dcl.fabienb@18pixel.fr>
2019-02-07 09:02:43 +0100

    update DB

--- controleur/login.php
+++ controleur/login.php
@@ -3,24 +3,37 @@ if(!empty($_POST) && !empty($_POST['userName']) && !empty($_POST['userPassword']
 
 require_once '../modele/pdo.php';
 
-$req = $pdo->prepare('SELECT * FROM Utilisateurs WHERE (NomUtilisateur = :nomMembre)');
+$req = $pdo->prepare('SELECT * FROM utilisateurs WHERE (NomUtilisateur = :nomMembre)');
 $req->execute(['nomMembre' => $_POST['userName']]);
 $user = $req->fetch(); // Récupère l'utilisateur
-session_start();
 
-if(password_verify($_POST['userPassword'], $user->motDePasseMembre)){
+if($_POST['userPassword'] == $user->MotDePasse) {
+    session_start();
 
-$_SESSION['auth'] = $user;
-$_SESSION['flash']['success'] = 'Vous etes maintenant bien connecté';
-header('Location: account.php');
-exit();
+    $_SESSION['auth'] = $user;
+    $_SESSION['flash']['success'] = 'Vous etes maintenant bien connecté';
+    header('Location: account.php');
+    exit();
 
 } else {
 
-$_SESSION['flash']['danger'] = "Heu vous avez fait une erreur d'email ou de mot de passe";
-header('Location: login.php');
-exit();
+    $_SESSION['flash']['danger'] = "Heu vous avez fait une erreur d'email ou de mot de passe";
+    header('Location: ../index.php');
+    exit();
 
 }
 }
 ?>
+<div class="container">
+    <?php if(isset($_SESSION['flash'])): ?> <!----- Gère les messages d'erreurs ----------->
+    <?php foreach($_SESSION['flash'] as $type => $message): ?>
+
+    <div class="alert alert-<?= $type; ?>">
+        <?= $message; ?>
+    </div>
+
+    <?php endforeach; ?>
+    <?php unset($_SESSION['flash']); ?> <!--------- Supprime le message d'erreur ---------------->
+    <?php endif; ?>
+
+</div> 
--- modele/pdo.php
+++ modele/pdo.php
@@ -3,9 +3,10 @@
 try {
 $json =file_get_contents('../modele/pdo.json');
 $decode=json_decode($json, true);
-$pdo = new PDO("mysql:host:3306=".$decode['host'].";dbname=".$decode['dbName'], $decode['user'], $decode['pass']);
+$pdo = new PDO("mysql:host=".$decode['host'].";dbname=".$decode['dbName'], $decode['user'], $decode['pass']);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
 $pdo->exec('SET NAMES utf8');
     
 
