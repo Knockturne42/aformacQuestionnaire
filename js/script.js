e26399603838ab0ed788316ef980de0eb9c0ff12
DclFabienLycan <dcl.fabienb@18pixel.fr>
2019-02-11 15:17:47 +0100

    questionnaire

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
-const ajouter = document.getElementById('ajout');
+// const ajouter = document.getElementById('ajout');
 
-ajouter.addEventListener("click", function () {
-    console.log('coucou');
-    var makeInput = document.createElement('div');
-    makeInput.setAttribute('class', "question");
-    ajouter.appendChild(makeInput);
-    makeInput.style.height = "150" + "px";
-    makeInput.style.width = "150" + "px";
-    makeInput.style.backgroundColor = "blue";
+// ajouter.addEventListener("click", function () {
+//     var makeInput = document.createElement('div');
+//     makeInput.setAttribute('class', "question");
+//     ajouter.appendChild(makeInput);
+//     makeInput.style.height = "150" + "px";
+//     makeInput.style.width = "150" + "px";
+//     makeInput.style.backgroundColor = "blue";
+// })
+
+const repPlus = document.getElementsByClassName("repPlus");
+var rep = document.getElementsByClassName('reponse');
+var test = document.getElementById("toto");
+const zoneQuestPlus = document.getElementsByClassName('zoneQuestPlus');
+const zoneQuestRep = document.getElementById('zoneQuestRep');
+plus(repPlus[0], test);
+
+zoneQuestPlus[0].addEventListener("click", function () {
+    var zonePlus = document.createElement('div');
+    zonePlus.setAttribute('id', "toto");
+    zoneQuestRep.insertBefore(zonePlus, null);
+
+    var question = document.createElement('div');
+    question.setAttribute('class', "question");
+    zonePlus.appendChild(question);
+
+    var reponse = document.createElement('div');
+    reponse.setAttribute('class', "reponse");
+    zonePlus.appendChild(reponse);
+
+    var repPlus = document.createElement('div');
+    repPlus.setAttribute('class', "repPlus");
+    zonePlus.appendChild(repPlus);
+    plus(repPlus, zonePlus);
 })
+
+
+
+function plus (target, parents) {
+    target.addEventListener("click", function () {
+        var reponse = document.createElement('div');
+        reponse.setAttribute('class', "reponse");
+        parents.insertBefore(reponse, target);
+    });
+};
--- vue/insertion.php
+++ vue/insertion.php
@@ -7,13 +7,30 @@ include '../controleur/class/input.php';
 
 ?>
 
+    <div class="container" id="ajout">
         <form action="" method="POST">
-            <input type="text" id="nomQuestionnaire" name="nomQuestionnaire" placeholder="Nom du Questionnaire">
-            <button type="submit">Poster le Questionnaire</button>
-            <div class="container" id="ajout">
-            
+            <div>
+                <input type="text" id="nomQuestionnaire" name="nomQuestionnaire" placeholder="Nom du Questionnaire">
             </div>
+            <br>
+            <div id="zoneQuestRep">
+                <div id="toto">
+                    <div class="question">
+                        <input type="text" placeholder="Question">
+                    </div>
+                    <br>
+                    <div class="reponse">
+                        <!-- Mettre ici le select + la génération de l'input suivant le type, à la place de celui en place -->
+                        <input type="text" placeholder="Réponse">
+                    </div>
+                    <div class="repPlus">+</div>
+                </div>
+            </div>
+            <div class="zoneQuestPlus">+</div>
+            <br>
+            <button type="submit">Poster le Questionnaire</button>
         </form>
+    </div>
     </body>
     <script src="../js/script.js"></script>
 </html>
